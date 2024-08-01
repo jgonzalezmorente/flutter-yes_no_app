396d54f3d8013897ec0f7f170e2d36beb50992f9
jgonzalezmorente <jgonzalezmorente@gmail.com>
2024-08-01 10:48:25 +0200

    Primer commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://indiehoy.com/wp-content/uploads/2019/10/rachel-friends-1200x800.jpg'),
+          ),
+        ),
+        title: const Text('Mi amor'),
+        centerTitle: false,
+      ),
+      body: _ChatView()
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return ( index % 2 == 0)
+                    ? const MyMessageBubble()
+                    : const HerMessageBubble();                   
+                }
+              )
+            ),
+            ///* Caja de texto de mensajes
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
